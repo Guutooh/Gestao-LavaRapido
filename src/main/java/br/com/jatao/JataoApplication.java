1842b139d41fdd1cbbd418c70cb8d5eeb47391bd
guutooh <gustavo.oliveiradossantos@gmail.com>
2024-05-17 17:27:06 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/jatao/JataoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jatao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JataoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JataoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jatao/JataoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jatao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JataoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
